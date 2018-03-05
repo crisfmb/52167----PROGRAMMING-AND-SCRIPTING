c95b1a0c0076361598c6d3abe0b710df73e22b07
Cristina Borges <crisfmb@gmail.com>
2018-03-05 12:57:47 +0000

    Add files via upload
    Week 3

---
+++ collatz.py
@@ -0,0 +1,18 @@
+# Cristina Borges, 09-Feb-18
+
+def collatz(num):
+    while num != 1:
+        print(num)
+        if num % 2 == 0:
+            num = int(num / 2)
+        else:
+            num = int(3 * num + 1)
+    else:
+        print(num)
+        print('Done :-)')
+
+def main():
+    num = int(input('Input an integer: '))
+    collatz(num)
+
+main()
