cc05b18b50a266fa607f3ccdb56b834ad58100c4
Cristina Borges <crisfmb@gmail.com>
2018-03-24 13:34:08 +0000

    Update collatz.py
    change the code coment

--- collatz.py
+++ collatz.py
@@ -1,9 +1,9 @@
 # Cristina Borges, 09-Feb-18
 
-def collatz(num):
+def collatz(num): # Condition while input number was different then 1
     while num != 1:
         print(num)
-        if num % 2 == 0:
+        if num % 2 == 0: # Check if the number is an even or odd.
             num = int(num / 2)
         else:
             num = int(3 * num + 1)
@@ -12,7 +12,7 @@ def collatz(num):
         print('Done :-)')
 
 def main():
-    num = int(input('Input an integer: '))
+    num = int(input('Input an integer: ')) # enter a number
     collatz(num)
 
 main()
